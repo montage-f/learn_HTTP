435a86263cf101eb5221b35629d0c4fe82a0e306
fanzheng <montage_fz@sina.com>
2019-04-09 16:48:41 +0800

    - 浏览器跨域限制

--- 02. 理解跨域/02. CORS跨域限制以及预请求验证_8080.js
+++ 02. 理解跨域/02. CORS跨域限制以及预请求验证_8080.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-	const html = fs.readFileSync('02. port_8080.html', 'utf8');
+	const html = fs.readFileSync('02. CORS跨域限制以及预请求验证_8080.html', 'utf8');
 	res.writeHead(200, {
 		'Content-Type': 'text/html'
 	});
