c3e322257648a803683071c684ebf5b7a8b10262
fanzheng <montage_fz@sina.com>
2019-04-09 16:53:59 +0800

    - 缓存机制

---
+++ 03. 缓存Cache-Control.js
@@ -0,0 +1,13 @@
+/**
+ * Created by MonTage_fz on 2019/4/9
+ */
+// 可缓存性
+// public => http经过的任何一个代理服务器都可以进行缓存
+
+// private => 只允许当前发出请求的浏览器进行缓存
+
+// no-cache => 不允许缓存
+
+// 到期
+// max-age=<seconds>
+
